33620ef173f7b80c8401f30ea27eff2940b751ba
satinder-2000 <ssingh.2023@gmail.com>
2026-08-06 00:28:43 +0100

    nightly commit

--- tensorflow/official/keras/TheFunctionalAPI.py
+++ tensorflow/official/keras/TheFunctionalAPI.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
+from tensorflow.python.tpu.feature_column import shared_embedding_columns
 
 inputs = keras.Input(shape=(784,))
 print("inputs.shape: ",inputs.shape)
@@ -156,3 +157,100 @@ model = keras.Model(
 )
 
 keras.utils.plot_model(model, "multi_input_and_output_model.png", show_shapes=True)
+
+print("-----Compile the model-----")
+print()
+model.compile(
+    optimizer=keras.optimizers.RMSprop(1e-3),
+    loss=[
+        keras.losses.BinaryCrossentropy(from_logits=True),
+        keras.losses.CategoricalCrossentropy(from_logits=True),
+    ],
+    loss_weights=[1.0, 0.2]
+)
+
+print("-----Train the model by passing lists of NumPy arrays of inputs and targets:----")
+# Dummy input data
+title_data = np.random.randint(num_words, size=(1280, 10))
+body_data = np.random.randint(num_words, size=(1280, 100))
+tags_data = np.random.randint(2, size=(1280, num_tags)).astype("float32")
+
+# Dummy target data
+priority_targets = np.random.random(size=(1280, 1))
+dept_targets = np.random.randint(2, size=(1280, num_departments))
+
+model.fit(
+    {"title": title_data, "body": body_data, "tags": tags_data},
+    {"priority": priority_targets, "department": dept_targets},
+    epochs=2,
+    batch_size=32,
+)
+print()
+print("-----A toy ResNet model----")
+print()
+inputs = keras.Input(shape=(32, 32, 3), name="img")
+x = layers.Conv2D(32, 3, activation="relu")(inputs)
+x = layers.Conv2D(64, 3, activation="relu")(x)
+block_1_output = layers.MaxPooling2D(3)(x)
+
+x = layers.Conv2D(64, 3, activation="relu", padding="same")(block_1_output)
+x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+block_2_output = layers.add([x, block_1_output])
+
+x = layers.Conv2D(64, 3, activation="relu", padding="same")(block_2_output)
+x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+block_3_output = layers.add([x, block_2_output])
+
+x = layers.Conv2D(64, 3, activation="relu")(block_3_output)
+x = layers.GlobalAveragePooling2D()(x)
+x = layers.Dense(256, activation="relu")(x)
+x = layers.Dropout(0.5)(x)
+outputs = layers.Dense(10)(x)
+
+model = keras.Model(inputs, outputs, name="toy_resnet")
+print("model.summary():\n",model.summary())
+
+#plot the model
+keras.utils.plot_model(model, "mini_resnet.png", show_shapes=True)
+
+print()
+print("---Now train the model---")
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+model.compile(
+    optimizer=keras.optimizers.RMSprop(1e-3),
+    loss=keras.losses.CategoricalCrossentropy(from_logits=True),
+    metrics=["acc"],
+)
+# We restrict the data to the first 1000 samples so as to limit execution time
+# on Colab. Try to train on the entire dataset until convergence!
+model.fit(x_train[:1000], y_train[:1000], batch_size=64, epochs=1, validation_split=0.2)
+
+print("-----Shared layers-----")
+print()
+shared_embedding = layers.Embedding(1000, 128)
+text_input_a = keras.Input(shape=(None,), dtype="int32")
+text_input_b = keras.Input(shape=(None,), dtype="int32")
+
+encoded_input_a = shared_embedding(text_input_a)
+encoded_input_b = shared_embedding(text_input_b)
+
+print("---Extract and reuse nodes in the graph of layers---")
+print()
+vgg19 = keras.applications.VGG19()
+
+features_list = [layer.output for layer in vgg19.layers]
+
+feat_extraction_model = keras.Model(inputs=vgg19.input, outputs=features_list)
+
+img = np.random.random((1, 224, 224, 3)).astype("float32")
+extracted_features = feat_extraction_model(img)
+print("extracted_features:\n",extracted_features)
+print()
+print("-----Extend the API using custom layers-----")
+print()
---
+++ tensorflow/official/keras/Training & evaluation with the built-in methods.py
@@ -0,0 +1,230 @@
+import tensorflow as tf
+import keras
+from keras import layers
+
+print("-----API overview: a first end-to-end example-----")
+print()
+inputs = keras.Input(shape=(784,), name="digits")
+x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
+x = layers.Dense(64, activation="relu", name="dense_2")(x)
+outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train.reshape(60000, 784).astype("float32") / 255
+x_test = x_test.reshape(10000, 784).astype("float32") / 255
+
+y_train = y_train.astype("float32")
+y_test = y_test.astype("float32")
+
+# Reserve 10,000 samples for validation
+x_val = x_train[-10000:]
+y_val = y_train[-10000:]
+x_train = x_train[:-10000]
+y_train = y_train[:-10000]
+
+model.compile(
+    optimizer=keras.optimizers.RMSprop(),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[keras.metrics.SparseCategoricalAccuracy()]
+)
+
+print("Fit model on training data")
+history = model.fit(
+    x_train,
+    y_train,
+    batch_size=64,
+    epochs=2,
+    # We pass some validation for
+    # monitoring validation loss and metrics
+    # at the end of each epoch
+    validation_data=(x_val, y_val),
+)
+print("history.history:\n",history.history)
+print()
+print("We evaluate the model on the test data via evaluate()")
+print()
+results = model.evaluate(x_test, y_test, batch_size=128)
+print("test loss, test acc:", results)
+print("Generate predictions for 3 samples")
+predictions = model.predict(x_test[:3])
+print("predictions shape:", predictions.shape)
+print()
+print("-----The compile() method: specifying a loss, metrics, and an optimizer-----")
+print()
+model.compile(
+    optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[keras.metrics.SparseCategoricalAccuracy()]
+)
+
+def get_uncompiled_model():
+    inputs = keras.Input(shape=(784,), name="digits")
+    x = layers.Dense(64, activation="relu", name="dense_1")(inputs)
+    x = layers.Dense(64, activation="relu", name="dense_2")(x)
+    outputs = layers.Dense(10, activation="softmax", name="predictions")(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    return model
+
+
+def get_compiled_model():
+    model = get_uncompiled_model()
+    model.compile(
+        optimizer="rmsprop",
+        loss="sparse_categorical_crossentropy",
+        metrics=["sparse_categorical_accuracy"],
+    )
+    return model
+
+def custom_mean_squared_error(y_true, y_pred):
+    return tf.math.reduce_mean(tf.square(y_true - y_pred), axis=-1)
+
+
+model = get_uncompiled_model()
+model.compile(optimizer=keras.optimizers.Adam(), loss=custom_mean_squared_error)
+
+# We need to one-hot encode the labels to use MSE
+y_train_one_hot = tf.one_hot(y_train, depth=10)
+model.fit(x_train, y_train_one_hot, batch_size=64, epochs=1)
+print()
+print("---subclass the keras.losses.Loss class---")
+@keras.saving.register_keras_serializable()
+class CustomMSE(keras.losses.Loss):
+    def __init__(self, regularization_factor=0.1, name="custom_mse"):
+        super().__init__(name=name)
+        self.regularization_factor = regularization_factor
+
+    def call(self, y_true, y_pred):
+        mse = tf.math.reduce_mean(tf.square(y_true - y_pred), axis=-1)
+        reg = tf.math.reduce_mean(tf.square(0.5 - y_pred), axis=-1)
+        return mse + reg * self.regularization_factor
+
+    def get_config(self):
+        return {
+            "regularization_factor": self.regularization_factor,
+            "name": self.name,
+        }
+
+
+model = get_uncompiled_model()
+model.compile(optimizer=keras.optimizers.Adam(), loss=CustomMSE())
+
+y_train_one_hot = tf.one_hot(y_train, depth=10)
+model.fit(x_train, y_train_one_hot, batch_size=64, epochs=1)
+print()
+print("-----Custom metrics  subclassing the keras.metrics.Metric class-----")
+@keras.saving.register_keras_serializable()
+class CategoricalTruePositives(keras.metrics.Metric):
+    def __init__(self, name="categorical_true_positives", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.true_positives = self.add_weight(name="ctp", initializer="zeros")
+
+    def update_state(self, y_true, y_pred, sample_weight=None):
+        y_pred = tf.reshape(tf.argmax(y_pred, axis=1), shape=(-1, 1))
+        values = tf.cast(y_true, "int32") == tf.cast(y_pred, "int32")
+        values = tf.cast(values, "float32")
+        if sample_weight is not None:
+            sample_weight = tf.cast(sample_weight, "float32")
+            values = tf.multiply(values, sample_weight)
+        self.true_positives.assign_add(tf.reduce_sum(values))
+
+    def result(self):
+        return self.true_positives
+
+    def reset_state(self):
+        # The state of the metric will be reset at the start of each epoch.
+        self.true_positives.assign(0.0)
+
+
+model = get_uncompiled_model()
+model.compile(
+    optimizer=keras.optimizers.RMSprop(learning_rate=1e-3),
+    loss=keras.losses.SparseCategoricalCrossentropy(),
+    metrics=[CategoricalTruePositives()],
+)
+model.fit(x_train, y_train, batch_size=64, epochs=3)
+
+print()
+print("---Demo LogisticEndpoint layer---")
+print()
+
+@keras.saving.register_keras_serializable()
+class LogisticEndpoint(layers.Layer):
+    def __init__(self, name= None):
+        super().__init__(name=name)
+        self.loss_fn = keras.losses.BinaryCrossentropy(from_logits=True)
+
+    def call(self, targets, logits, sample_weights=None):
+        # Compute the training-time loss value and add it
+        # to the layer using `self.add_loss()`.
+        loss = self.loss_fn(targets, logits, sample_weight=sample_weights)
+        self.add_loss(loss)
+
+        # Return the inference-time prediction tensor (for `.predict()`).
+        return tf.nn.softmax(logits)
+
+
+print()
+print("---use it in a model with two inputs (input data & targets)---")
+print()
+import numpy as np
+
+inputs = keras.Input(shape=(3,), name="inputs")
+targets = keras.Input(shape=(10,), name="targets")
+logits = keras.layers.Dense(10)(inputs)
+predictions = LogisticEndpoint(name="predictions")(targets, logits)
+
+model = keras.Model(inputs=[inputs, targets], outputs=predictions)
+model.compile(optimizer="adam")
+
+data = {
+    "inputs": np.random.random((3, 3)),
+    "targets": np.random.random((3, 10)),
+}
+
+model.fit(data)
+
+print()
+print("---Automatically setting apart a validation holdout set---")
+print("the argument validation_split allows you to automatically reserve part of your training data for validation.")
+print()
+model=get_compiled_model()
+model.fit(x_train, y_train, batch_size=64, validation_steps=0.2, epochs=1)
+
+print()
+print("---Training & evaluation from tf.data Datasets---")
+print()
+model = get_compiled_model()
+
+# First, let's create a training Dataset instance.
+# For the sake of our example, we'll use the same MNIST data as before.
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+# Shuffle and slice the dataset.
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(64)
+
+# Now we get a test dataset.
+test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+test_dataset = test_dataset.batch(64)
+
+# Since the dataset already takes care of batching,
+# we don't pass a `batch_size` argument.
+model.fit(train_dataset, epochs=3)
+
+# You can also evaluate or predict on a dataset.
+print("Evaluate")
+result = model.evaluate(test_dataset)
+dict(zip(model.metrics_names, result))
+
+print()
+print("steps_per_epoch argument > how many training steps the model should run before moving on to the next epoch")
+print()
+model = get_compiled_model()
+
+# Prepare the training dataset
+train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_dataset = train_dataset.shuffle(buffer_size=1024).batch(64)
+
+# Only use the 100 batches per epoch (that's 64 * 100 samples)
+model.fit(train_dataset, epochs=3, steps_per_epoch=100)
