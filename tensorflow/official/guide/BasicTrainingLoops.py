37f0d6a390dfc01bfa1cfa3184e3e2072cff1261
satinder-2000 <ssingh.2023@gmail.com>
2026-08-16 00:26:48 +0100

    W.I.P

--- tensorflow/official/guide/BasicTrainingLoops.py
+++ tensorflow/official/guide/BasicTrainingLoops.py
@@ -10,3 +10,106 @@ print()
 print("-----Setup-----")
 print()
 import tensorflow as tf
+
+import matplotlib.pyplot as plt
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+#print(colors)
+
+print()
+print("-----Solving machine learning problems-----")
+print()
+print()
+print("-----Data-----")
+print()
+#The actual line
+TRUE_W = 3.0
+TRUE_B = 2.0
+
+NUM_EXAMPLES = 201
+
+#A vector of random x values
+x = tf.linspace(-2, 2, NUM_EXAMPLES)
+x = tf.cast(x, tf.float32)
+
+def f(x):
+    return x * TRUE_W * TRUE_B
+
+# Generate some noise
+noise = tf.random.normal(shape=[NUM_EXAMPLES])
+
+#Calculate y
+y = f(x) + noise
+
+#plot all the data
+plt.plot(x, y, '.')
+plt.show()
+print()
+print("-----Define the model-----")
+print()
+class MyModel(tf.Module):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # Initialize the weights to `5.0` and the bias to `0.0`
+        # In practice, these should be randomly initialized
+        self.w = tf.Variable(5.0)
+        self.b = tf.Variable(0.0)
+        
+    def __call__(self, x):
+        return self.w * x + self.b
+    
+
+model = MyModel()
+
+# List the variables tf.modules's built-in variable aggregation.
+print("Variables: ", model.variables)
+
+#Verify the model works
+assert model(3.0).numpy() == 15.0
+
+print()
+print("-----Define a loss function---")
+print()
+print("compute a single loss value for an entire batch")
+def loss(target_y, predicted_y):
+    return tf.reduce_mean(tf.square(target_y - predicted_y))
+
+print("before training the model, visulize the loss value")
+plt.plot(x, y, '.', label='Data')
+plt.plot(x, f(x), label='Ground truth')
+plt.plot(x, model(x), label='Predictions')
+plt.legend()
+plt.show()
+
+print("Current loss: %1.6f" % loss(y, model(x)).numpy())
+
+print()
+print("-----Define a training loop---")
+print()
+# Given a callable model, inputs, outputs, and a learning rate...
+def train(model, x, y, learning_rate):
+    
+    with tf.GradientTape() as t:
+        # Trainable variables are automatically tracked by GradientTape
+        current_loss = loss(y, model(x))
+
+        # Use GradientTape to calculate the gradients with respect to W and b
+        dw, db = t.gradient(current_loss,[model.w, model.b])
+
+        # Subtract the gradient scaled by the learning rate        
+        model.w.assign_sub(learning_rate * dw)
+        model.b.assign_sub(learning_rate * db)
+        
+
+model = MyModel()
+
+# Collect the history of W-values and b-values to plot later
+weights = []
+biases = []
+epoches = range(10)
+
+# Define a training loop
+def report(model, loss):
+    return f"W = {model.w.numpy():1.2f}, b ={model.b.numpy(): 1.2f},loss ={loss:2.5f}"
+
+
