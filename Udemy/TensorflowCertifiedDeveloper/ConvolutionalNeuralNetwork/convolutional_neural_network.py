d038ce89292a037eebc92b9bd7e3597072dd9069
satinder-2000 <satinder_2000@outlook.com>
2026-08-20 00:10:55 +0100

    spyder ini

---
+++ Udemy/TensorflowCertifiedDeveloper/ConvolutionalNeuralNetwork/convolutional_neural_network.py
@@ -0,0 +1,5 @@
+print("Source:https://drive.google.com/drive/folders/1G-P6sC2n82EHBrXqNjwz5tIpRSkDB99j")
+print("Importing the libraries")
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+print(tf.__version__)
